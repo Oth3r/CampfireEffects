d28a167c327a73ce230f19e2922994f884e0cbbd
OtherOne <oth3rone@outlook.com>
2020-07-27 13:24:05 -0500

    Custom config

---
+++ src/me/otherone/campfire/yml/addConfig.java
@@ -0,0 +1,18 @@
+package me.otherone.campfire.yml;
+
+public class addConfig {
+
+    public static void start() {
+        if (Config.get().getString("config-version") == null) {
+            Config.get().set("config-version", 1.0);
+            Config.get().set("check-ticks", 20);
+            Config.get().set("campfire.effect", "REGENERATION");
+            Config.get().set("campfire.level", 1);
+            Config.get().set("campfire.radius", 10);
+            Config.get().set("soul-campfire.effect", "DAMAGE_RESISTANCE");
+            Config.get().set("soul-campfire.level", 1);
+            Config.get().set("soul-campfire.radius", 10);
+            Config.save();
+        }
+    }
+}
