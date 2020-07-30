3a5237ffbe6ad455314893a5e74efe033f4e7259
OtherOne <oth3rone@outlook.com>
2020-07-30 10:47:18 -0500

    Fixed event

--- src/me/otherone/campfire/CampfireEvents.java
+++ src/me/otherone/campfire/CampfireEvents.java
@@ -10,8 +10,7 @@ public class CampfireEvents implements Listener {
     @EventHandler
     public static void onPlayerDisconnect(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        if (Utl.campfirePlayer(player)) {
-            Utl.removeCampfirePlayer(player);
-        }
+        Utl.removeCampfirePlayer(player);
+        Utl.removeSoulCampfirePlayer(player);
     }
 }
