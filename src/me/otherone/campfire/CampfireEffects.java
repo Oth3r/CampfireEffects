d17d070ac18aad09ef893c94c7c6f6a4518eb290
OtherOne <oth3rone@outlook.com>
2020-07-30 10:19:40 -0500

    Fixed config loading

--- src/me/otherone/campfire/CampfireEffects.java
+++ src/me/otherone/campfire/CampfireEffects.java
@@ -1,5 +1,7 @@
 package me.otherone.campfire;
 
+import me.otherone.campfire.yml.Config;
+import me.otherone.campfire.yml.addConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,6 +10,8 @@ public class CampfireEffects extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Config.setup();
+        addConfig.start();
         getServer().getPluginManager().registerEvents(new CampfireEvents(), this);
         FindCampfire.run(this);
     }
@@ -18,10 +22,10 @@ public class CampfireEffects extends JavaPlugin {
             Utl.removeCampfirePlayer(player);
             Utl.removeSoulCampfirePlayer(player);
         }
-
+        if (Config.get().getString("campfire-players") != null)
+            Config.get().set("campfire-players", null);
+        if (Config.get().getString("soulcampfire-players") != null)
+            Config.get().set("soulcampfire-players", null);
     }
-
-    //TODO
-    // make check ticks config option work
 
 }
