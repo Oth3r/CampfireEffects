6fbefb0fbd91ba6bb1f4330bc5a08a58090ab372
OtherTablet <68134921+Oth3r@users.noreply.github.com>
2020-08-01 21:36:46 -0500

    remove potion effect on death

--- src/me/otherone/campfire/CampfireEvents.java
+++ src/me/otherone/campfire/CampfireEvents.java
@@ -3,6 +3,7 @@ package me.otherone.campfire;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class CampfireEvents implements Listener {
@@ -10,6 +11,13 @@ public class CampfireEvents implements Listener {
     @EventHandler
     public static void onPlayerDisconnect(PlayerQuitEvent event) {
         Player player = event.getPlayer();
+        Utl.removeCampfirePlayer(player);
+        Utl.removeSoulCampfirePlayer(player);
+    }
+
+    @EventHandler
+    public static void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
         Utl.removeCampfirePlayer(player);
         Utl.removeSoulCampfirePlayer(player);
     }
