e1bdd02dfbfa651ab3abc1dcbd59e20ac0acb612
OtherTablet <68134921+Oth3r@users.noreply.github.com>
2020-08-01 21:37:07 -0500

    not done

--- src/me/otherone/campfire/Utl.java
+++ src/me/otherone/campfire/Utl.java
@@ -11,6 +11,120 @@ import java.util.List;
 
 public class Utl {
 
+    //speed, slowness, haste, mining_fatigue, strength,
+    //instant_health, instant_damage, jump_boost, nausea,
+    //regeneration, resistance, fire_resistance, water_breathing,
+    //invisibility, blindness, night_vision, hunger, weakness,
+    //poison, wither, health_boost, absorption, saturation,
+    //glowing, levitation, luck, unluck, slow_falling,
+    //conduit_power, dolphins_grace, bad_omen, hero_of_the_village
+
+    private static PotionEffectType getEffect(String simple) {
+        if (simple.equalsIgnoreCase("speed")) {
+            return PotionEffectType.SPEED;
+        }
+        if (simple.equalsIgnoreCase("slowness")) {
+            return PotionEffectType.SLOW;
+        }
+        if (simple.equalsIgnoreCase("haste")) {
+            return PotionEffectType.FAST_DIGGING;
+        }
+        if (simple.equalsIgnoreCase("mining_fatigue")) {
+            return PotionEffectType.SLOW_DIGGING;
+        }
+        if (simple.equalsIgnoreCase("strength")) {
+            return PotionEffectType.INCREASE_DAMAGE;
+        }
+        //instant_health, instant_damage, jump_boost, nausea,
+        if (simple.equalsIgnoreCase("instant_health")) {
+            return PotionEffectType.HEAL;
+        }
+        if (simple.equalsIgnoreCase("instant_damage")) {
+            return PotionEffectType.HARM;
+        }
+        if (simple.equalsIgnoreCase("jump_boost")) {
+            return PotionEffectType.JUMP;
+        }
+        if (simple.equalsIgnoreCase("nausea")) {
+            return PotionEffectType.CONFUSION;
+        }
+        //regeneration, resistance, fire_resistance, water_breathing,
+        if (simple.equalsIgnoreCase("regeneration")) {
+            return PotionEffectType.REGENERATION;
+        }
+        if (simple.equalsIgnoreCase("resistance")) {
+            return PotionEffectType.DAMAGE_RESISTANCE;
+        }
+        if (simple.equalsIgnoreCase("fire_resistance")) {
+            return PotionEffectType.FIRE_RESISTANCE;
+        }
+        if (simple.equalsIgnoreCase("water_breathing")) {
+            return PotionEffectType.WATER_BREATHING;
+        }
+        //invisibility, blindness, night_vision, hunger, weakness,
+        if (simple.equalsIgnoreCase("invisibility")) {
+            return PotionEffectType.INVISIBILITY;
+        }
+        if (simple.equalsIgnoreCase("blindness")) {
+            return PotionEffectType.BLINDNESS;
+        }
+        if (simple.equalsIgnoreCase("night_vision")) {
+            return PotionEffectType.NIGHT_VISION;
+        }
+        if (simple.equalsIgnoreCase("hunger")) {
+            return PotionEffectType.HUNGER;
+        }
+        if (simple.equalsIgnoreCase("weakness")) {
+            return PotionEffectType.WEAKNESS;
+        }
+        //poison, wither, health_boost, absorption, saturation,
+        if (simple.equalsIgnoreCase("poison")) {
+            return PotionEffectType.POISON;
+        }
+        if (simple.equalsIgnoreCase("wither")) {
+            return PotionEffectType.WITHER;
+        }
+        if (simple.equalsIgnoreCase("health_boost")) {
+            return PotionEffectType.HEALTH_BOOST;
+        }
+        if (simple.equalsIgnoreCase("absorption")) {
+            return PotionEffectType.ABSORPTION;
+        }
+        if (simple.equalsIgnoreCase("saturation")) {
+            return PotionEffectType.SATURATION;
+        }
+        //glowing, levitation, luck, unluck, slow_falling,
+        if (simple.equalsIgnoreCase("glowing")) {
+            return PotionEffectType.GLOWING;
+        }
+        if (simple.equalsIgnoreCase("levitation")) {
+            return PotionEffectType.LEVITATION;
+        }
+        if (simple.equalsIgnoreCase("luck")) {
+            return PotionEffectType.LUCK;
+        }
+        if (simple.equalsIgnoreCase("unluck")) {
+            return PotionEffectType.UNLUCK;
+        }
+        if (simple.equalsIgnoreCase("slow_falling")) {
+            return PotionEffectType.SLOW_FALLING;
+        }
+        //conduit_power, dolphins_grace, bad_omen, hero_of_the_village
+        if (simple.equalsIgnoreCase("conduit_power")) {
+            return PotionEffectType.CONDUIT_POWER;
+        }
+        if (simple.equalsIgnoreCase("dolphins_grace")) {
+            return PotionEffectType.DOLPHINS_GRACE;
+        }
+        if (simple.equalsIgnoreCase("bad_omen")) {
+            return PotionEffectType.BAD_OMEN;
+        }
+        if (simple.equalsIgnoreCase("hero_of_the_village")) {
+            return PotionEffectType.HERO_OF_THE_VILLAGE;
+        }
+        //SIMPLEIFY THE CORRECT ONES
+    }
+
     public static String addColor(final String string) {
         return org.bukkit.ChatColor.translateAlternateColorCodes('&', string);
     }
